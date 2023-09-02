778a2eb3741fb3046437bc056e1b711a5f165a45
allanbraquiel <30609332+allanbraquiel@users.noreply.github.com>
2023-09-02 08:25:53 -0300

    Adicionando o seletor de frutas

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,12 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Acovado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
+
+# Vamos colocar uma lista de seleção aqui para que eles possam escolher as frutas que desejam incluir 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) 
+
+# display da tabela na pagina
 streamlit.dataframe(my_fruit_list)
